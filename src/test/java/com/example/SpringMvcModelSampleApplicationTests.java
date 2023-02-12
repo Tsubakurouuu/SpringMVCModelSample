ccab13d7c608ef6ecb30da2a7cb7896e1e8a232d
Tsubakurouuu <yzpolice2896@icloud.com>
2023-02-13 08:25:50 +0900

    Spring Freamwork超入門 ~やさしくわかるWEBアプリ開発~ 6章 6-2

---
+++ src/test/java/com/example/SpringMvcModelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcModelSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
